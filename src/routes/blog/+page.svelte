4c4169790b73a4abf865c0c38fa3369efc19806e
Kival Mahadew <kivalm@protonmail.com>
2024-04-06 15:26:00 +0200

    Coming Soon page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+    import NavbarPadding from "$lib/components/NavbarPadding.svelte";
+</script>
+
 <svelte:head>
     <title>Blog</title>
     <meta
@@ -14,3 +19,20 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://kivalm.com/blog" />
 </svelte:head>
+
+<NavbarPadding />
+<section class="bg-zinc-900 min-h-screen">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1
+                class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-500"
+            >
+                Coming Soon
+            </h1>
+            <p class="mb-4 text-3xl tracking-tight font-bold text-white">
+                This page is under construction
+            </p>
+            <Button href="/">Go back home</Button>
+        </div>
+    </div>
+</section>
