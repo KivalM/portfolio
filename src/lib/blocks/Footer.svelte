41a029b7c78508a457c4e3d54ca6e42955b9efd4
Kival Mahadew <kivalm@protonmail.com>
2024-04-07 02:39:49 +0200

    Update Footer text color

--- src/lib/blocks/Footer.svelte
+++ src/lib/blocks/Footer.svelte
@@ -2,7 +2,7 @@
     <div
         class="bg-zinc-800 rounded-lg w-full mx-auto container p-4 md:flex md:items-center md:justify-between"
     >
-        <span class="text-sm text-gray-500 sm:text-center"
+        <span class="text-sm text-gray-400 sm:text-center"
             >© 2024 <a
                 href="https://flowbite.com/"
                 class="hover:underline text-blue-400">Kival Mahadew</a
