6224de11c75236c9228d15012a9b2c2f74c0f4b4
Kival Mahadew <kivalm@protonmail.com>
2024-07-06 11:48:31 +0200

    prerender all

--- src/routes/blog/[...slug]/+page.ts
+++ src/routes/blog/[...slug]/+page.ts
@@ -1,7 +1,23 @@
 import { posts } from "$lib/blog/Blog";
+import type { EntryGenerator } from './$types';
 
 export const prerender = true;
+
 export const load = async ({ params }) => {
     let post = posts.find(post => post.slug === params.slug);
     return post;
 }
+
+export const entries: EntryGenerator = () => {
+    return posts.map(post => ({
+        slug: post.slug,
+        type: 'page',
+        title: post.title,
+        description: post.description,
+        date: post.date,
+        tags: post.tags,
+        url: post.url,
+        pin: post.pin
+    }));
+};
+
