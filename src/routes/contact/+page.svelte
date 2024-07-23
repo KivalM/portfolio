9913d85c6cc28d90f1e8b0b43eff2bc02dc229ac
Kival Mahadew <kivalm@protonmail.com>
2024-07-23 13:48:17 +0200

    Update font family for headings in blog, contact, projects, and work pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import Contact from './Contact.svelte';
 </script>
 
 <section>
 	<h2 class="text-6xl font-kode text-center">Contact Me</h2>
 	<hr class="border-2 border-primary w-1/5 mx-auto my-5" />
-	<h3 class="text-3xl font-sans text-center">(¬‿¬)</h3>
+	<h3 class="text-3xl font-jp text-center">(ง •̀_•́)ง</h3>
 
 	<!-- download cv -->
 </section>
