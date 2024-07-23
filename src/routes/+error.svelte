c626926b740345042126c915e0ec91f6801b7369
Kival Mahadew <kivalm@protonmail.com>
2024-07-23 13:01:23 +0200

    Add 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,13 @@
 <script>
+	import Pill from '$lib/components/button/Pill.svelte';
 	import Navbar from '$lib/components/navigation/navbar.svelte';
 </script>
 
 <Navbar />
-
-<div class="flex flex-col items-center justify-center h-screen">
-	<h1 class="text-4xl font-bold text-primary">404</h1>
+<div class="container mx-auto flex flex-col flex-grow items-center justify-center gap-4">
+	<h1 class="text-4xl font-bold text-base-content font-kode">404</h1>
 	<p>Page not found</p>
+	<a href="/">
+		<Pill name="Go Home" icon="mdi:home" />
+	</a>
 </div>
