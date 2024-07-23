abb0453aac6870d99d95d9d1cf30160099ffd289
Kival Mahadew <kivalm@protonmail.com>
2024-07-23 14:25:13 +0200

    refactor: Update footer link to personal website

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,8 +3,8 @@
 		class="bg-base-200 rounded-lg w-full mx-auto container p-4 md:flex md:items-center md:justify-between"
 	>
 		<span class="text-sm sm:text-center"
-			>© 2024 <a href="https://flowbite.com/" class="hover:underline text-primary">Kival Mahadew</a
-			>. All Rights Reserved.
+			>© 2024 <a href="https://kivalm.com/" class="hover:underline text-primary">Kival Mahadew</a>.
+			All Rights Reserved.
 		</span>
 		<ul class="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0"></ul>
 	</div>
