026dc9ba8df2baa5d68ee09aa1c7d2409fa4ad6e
Kival Mahadew <kivalm@protonmail.com>
2024-07-23 13:20:59 +0200

    refactor: Add prerendering to blog page

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,6 +1,8 @@
 // src/routes/+page.server.ts
 import { posts } from '$lib/blog/Blog';
 
+export const prerender = true;
+
 export const load = async ({ params }) => {
     return { posts: posts };
 }
