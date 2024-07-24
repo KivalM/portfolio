294301db162ca465099a02ad9b88c7994fabf011
Kival Mahadew <kivalm@protonmail.com>
2024-07-24 02:58:17 +0200

    refactor: remove logs

--- src/routes/blog/[...slug]/+page.ts
+++ src/routes/blog/[...slug]/+page.ts
@@ -5,14 +5,12 @@ export const prerender = true;
 
 export const load = async ({ params }) => {
     let post = posts[params.slug.toLowerCase()];
-    console.log(post);
     // if post not found then look in series
     if (!post) {
         for (const [key, value] of Object.entries(posts)) {
             if ('posts' in value) {
                 for (const subpost of value.posts) {
                     if (subpost.slug === params.slug.toLowerCase()) {
-                        console.log(subpost);
                         post = subpost;
                     }
                 }
